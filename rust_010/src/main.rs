06a5e08eae7dd6c429ac4878e42c2a3ac307cd22
ypwxxx <shiwanfan@163.com>
2023-12-28 20:42:54 +0800

    添加010，包与crate

---
+++ rust_010/src/main.rs
@@ -0,0 +1,16 @@
+// 包与crate
+
+fn main() {
+    println!("Hello, rust_010!");
+
+    //NOTE - crate是最小编译单元
+    //NOTE - crate有二进制和库两种形式，其中库类似lib，二进制类比可执行程序。
+    //NOTE - 二进制crate必须要有一个main函数作为入口，库不需要。
+    //NOTE - 一个包有一或者多个crate，会有一个Cargo.toml阐述这些crate。
+    //NOTE - 包可以有多个二进制crate，但是最多只有一个库crate。
+    //NOTE - 包最少也有一个crate
+    //NOTE - 默认 src/main.rs 是一个与包同名的二进制crate。
+    //NOTE - 默认 src/lib.rs 是一个与包同名的库crate。
+    //NOTE - src/main.rs 与 src/lib.rs可以同时存在，同时编译。
+    //NOTE - src/bin 下存放一个个二进制crate。
+}
